ec08bd3cdcc047c7ae4a0510acdd0fc05c88ddd2
Sapar Sayat <saparsayat01@gmail.com>
2024-08-02 22:21:34 +0500

    fix: fix tailwind bugs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,10 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
-  return <h1>Home</h1>;
+  return (
+    <h1>
+      Home
+      <Button variant="destructive">Корзина</Button>
+    </h1>
+  );
 }
